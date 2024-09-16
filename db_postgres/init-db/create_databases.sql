498d5962fc42039f4a962a9faec38ad70f5c0819
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-09-17 01:10:03 +0200

    [db] first time works

--- db_postgres/init-db/create_databases.sql
+++ db_postgres/init-db/create_databases.sql
@@ -9,23 +9,15 @@ replace password_1 with the password of the first microservice user
 and so on for the other services, put the credentials same as in the .env files of the microservices
 
 to see effect you can do:
-docker exec -it db_postgres-db-1 psql -U main_user_you_have_set -d main_db
+docker exec -it db_postgres-db-1 psql -U <user_name_from_docker_compose_env_file>
+
+WARNING: if you change this later on, you lose all data in the database (can't login)
 */
 
 /* first microservice */
 -- Create a database
 CREATE DATABASE service_name_1;
 -- Create a user with a password
-CREATE USER 'user_name_1'@'localhost' IDENTIFIED BY 'password_1';
+CREATE USER user_name_1 WITH PASSWORD 'password_1';
 -- Grant privileges on the first database
-GRANT ALL PRIVILEGES ON service_name_1.* TO 'user_name_1'@'localhost';
-
-/* second microservice */
-CREATE DATABASE service_name_2;
--- Create a user with a password
-CREATE USER 'user_name_2'@'localhost' IDENTIFIED BY 'password_2';
--- Grant privileges on the second database
-GRANT ALL PRIVILEGES ON service_name_2.* TO 'user_name_2'@'localhost';
-
--- Apply changes
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON DATABASE service_name_1 TO user_name_1;
