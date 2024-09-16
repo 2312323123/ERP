dcc870458989bee05447f945a0cf6be4b11d38a2
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-09-17 01:24:31 +0200

    [db] explained

--- db_postgres/init-db/create_databases.sql
+++ db_postgres/init-db/create_databases.sql
@@ -10,6 +10,7 @@ and so on for the other services, put the credentials same as in the .env files 
 
 to see effect you can do:
 docker exec -it db_postgres-db-1 psql -U <user_name_from_docker_compose_env_file>
+(it works without password because local connections are trusted by default)
 
 WARNING: if you change this later on, you lose all data in the database (can't login)
 */
@@ -21,3 +22,13 @@ CREATE DATABASE service_name_1;
 CREATE USER user_name_1 WITH PASSWORD 'password_1';
 -- Grant privileges on the first database
 GRANT ALL PRIVILEGES ON DATABASE service_name_1 TO user_name_1;
+
+/* second microservice */
+-- Create a database
+CREATE DATABASE service_name_2;
+-- Create a user with a password
+CREATE USER user_name_2 WITH PASSWORD 'password_2';
+-- Grant privileges on the second database
+GRANT ALL PRIVILEGES ON DATABASE service_name_2 TO user_name_2;
+
+/* ... */
