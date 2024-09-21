88b20819b4befc3da3c723737bc7577cf2558913
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-09-21 23:34:06 +0200

    [auth_and_permissions] import schemas to modules

--- auth_and_permissions/src/account_creation_requests/account_creation_requests.module.ts
+++ auth_and_permissions/src/account_creation_requests/account_creation_requests.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountCreationRequestsService } from './account_creation_requests.service';
 import { AccountCreationRequestsController } from './account_creation_requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountCreationRequest } from './entities/account_creation_request.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AccountCreationRequest])],
   controllers: [AccountCreationRequestsController],
   providers: [AccountCreationRequestsService],
 })
--- auth_and_permissions/src/valid_jwt_caches/valid_jwt_caches.module.ts
+++ auth_and_permissions/src/valid_jwt_caches/valid_jwt_caches.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ValidJwtCachesService } from './valid_jwt_caches.service';
 import { ValidJwtCachesController } from './valid_jwt_caches.controller';
+import { ValidJwtCach } from './entities/valid_jwt_cach.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ValidJwtCach])],
   controllers: [ValidJwtCachesController],
   providers: [ValidJwtCachesService],
 })
