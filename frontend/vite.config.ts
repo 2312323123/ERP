1d6278237afbc0710c443dc5910feb1da007eba6
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-09-22 23:07:25 +0200

    [frontend] hot reload and custom port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  preview: {
+    port: 8080,
+    strictPort: true,
+host: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    // origin: "http://localhost:8080",
+    watch: {
+      usePolling: true,
+    },
+  },
+})
