d165ad7495fbeebd84e15991d09ee9696246fd42
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-09-22 23:28:08 +0200

    [frontend] essential configs for sanity

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    // origin: "http://localhost:8080",
+    watch: {
+      usePolling: true,
+    },
+  },
 })
