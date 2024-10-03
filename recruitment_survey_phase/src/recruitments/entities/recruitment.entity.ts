a51c8de0c15318cf1b68a923fd05d732f9ec496c
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-10-03 03:05:31 +0200

    [surveys] add recruitment entity body

--- recruitment_survey_phase/src/recruitments/entities/recruitment.entity.ts
+++ recruitment_survey_phase/src/recruitments/entities/recruitment.entity.ts
@@ -1,4 +1,19 @@
-import { Entity } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
 @Entity()
-export class Recruitment {}
+export class Recruitment {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  start_date_time: Date;
+
+  @Column({ length: 126 })
+  survey_sending_secret: string;
+
+  @Column({ type: 'text' })
+  grading_instruction: string;
+}
