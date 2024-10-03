521d5e0026d613af0c2aad7aabd1ed9dc0a9e1af
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-10-03 02:48:33 +0200

    [surveys] fix seeder to count first and then seed only if count is zero

--- recruitment_survey_phase/src/seeder/seeder.service.ts
+++ recruitment_survey_phase/src/seeder/seeder.service.ts
@@ -6,9 +6,20 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class SeederService {
   constructor(@InjectRepository(AcceptsSurvey) private acceptsSurveyRepository: Repository<AcceptsSurvey>) {}
+  async onModuleInit() {
+    await this.seedDatabase();
+  }
 
-  async seedAcceptsSurvey() {
-    const acceptsSurvey = this.acceptsSurveyRepository.create({ accepts_surveys: false });
-    await this.acceptsSurveyRepository.save(acceptsSurvey);
+  private async seedDatabase() {
+    const count = await this.acceptsSurveyRepository.count();
+    if (count === 0) {
+      const initialEntity = this.acceptsSurveyRepository.create({
+        accepts_surveys: false,
+      });
+      await this.acceptsSurveyRepository.save(initialEntity);
+      console.log('Initial entity added to the accepts_surveys table');
+    } else {
+      console.log('accepts_surveys Table already has data, no seeding required');
+    }
   }
 }
