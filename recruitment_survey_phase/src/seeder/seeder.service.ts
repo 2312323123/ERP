202650c4b0f5bedda5925532c5705b37b373b19a
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-10-05 03:10:03 +0200

    [surveys] initialize can_evaluate_surveys to false

--- recruitment_survey_phase/src/seeder/seeder.service.ts
+++ recruitment_survey_phase/src/seeder/seeder.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AcceptsSurvey } from 'src/accepts_surveys/entities/accepts_survey.entity';
+import { CanEvaluateSurvey } from 'src/can_evaluate_surveys/entities/can_evaluate_survey.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeederService {
-  constructor(@InjectRepository(AcceptsSurvey) private acceptsSurveyRepository: Repository<AcceptsSurvey>) {}
+  constructor(
+    @InjectRepository(AcceptsSurvey) private acceptsSurveyRepository: Repository<AcceptsSurvey>,
+    @InjectRepository(CanEvaluateSurvey) private canEvaluateSurveyRepository: Repository<CanEvaluateSurvey>,
+  ) {}
   async onModuleInit() {
     await this.seedDatabase();
   }
 
   private async seedDatabase() {
-    const count = await this.acceptsSurveyRepository.count();
-    if (count === 0) {
+    const acceptsSurveysCount = await this.acceptsSurveyRepository.count();
+    if (acceptsSurveysCount === 0) {
       const initialEntity = this.acceptsSurveyRepository.create({
         accepts_surveys: false,
       });
@@ -20,6 +24,17 @@ export class SeederService {
       console.log('Initial entity added to the accepts_surveys table');
     } else {
       console.log('accepts_surveys Table already has data, no seeding required');
+    }
+
+    const canEvaluateSurveysCount = await this.canEvaluateSurveyRepository.count();
+    if (canEvaluateSurveysCount === 0) {
+      const initialEntity = this.canEvaluateSurveyRepository.create({
+        can_evaluate_surveys: false,
+      });
+      await this.canEvaluateSurveyRepository.save(initialEntity);
+      console.log('Initial entity added to the can_evaluate_survey table');
+    } else {
+      console.log('can_evaluate_survey Table already has data, no seeding required');
     }
   }
 }
