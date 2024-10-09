4c72174b40f855d1d659558593a6591a15b29506
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-10-09 02:07:12 +0200

    [frontend] make survey settings save button sticky

--- frontend/src/pages/RecruitmentSurveyStagePages/SurveysSettings/SurveysSettings.tsx
+++ frontend/src/pages/RecruitmentSurveyStagePages/SurveysSettings/SurveysSettings.tsx
@@ -167,25 +167,9 @@ const SurveysSettings = () => {
       <Divider sx={{ my: 4 }} />
 
       <Container maxWidth="md">
-        {/* Settings Header */}
-        <Typography variant="h4" gutterBottom>
-          Ustawienia
-        </Typography>
-
-        <Typography variant="body1" color="textSecondary" gutterBottom>
-          (jak skończysz, kliknij 'Zapisz', albo się nie zapisze)
-        </Typography>
-
-        {/* Save Button */}
-        <Button variant="contained" color="primary" onClick={handleSave} fullWidth>
-          Zapisz
-        </Button>
-
-        <Divider sx={{ my: 2 }} />
-
         <Box my={3}>
           {/* List of editable fields */}
-          <Typography variant="h5" gutterBottom>
+          <Typography variant="h4" gutterBottom>
             Stwórz nową rekrutację
           </Typography>
 
@@ -215,7 +199,45 @@ const SurveysSettings = () => {
             Stwórz
           </Button>
         </Box>
+      </Container>
 
+      <Divider sx={{ my: 3 }} />
+
+      <Container
+        sx={{
+          position: 'sticky',
+          zIndex: 10,
+          top: '4rem',
+          background: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          border: '1px solid lightgray',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          padding: '0.75rem',
+        }}
+      >
+        <Box maxWidth="md" width="100%" display="flex" flexDirection={{ xs: 'column', md: 'row' }}>
+          {/* Left Column */}
+          <Box flex={1} flexBasis="75%">
+            <Typography variant="h4" gutterBottom>
+              Ustawienia aktywnej rekrutacji
+            </Typography>
+            <Typography variant="body1" color="textSecondary" gutterBottom>
+              (jak skończysz, kliknij 'Zapisz', albo się nie zapisze)
+            </Typography>
+          </Box>
+
+          {/* Right Column */}
+          <Box flex={1} display="flex" justifyContent="flex-end" flexBasis="25%">
+            <Button variant="contained" color="primary" onClick={handleSave} fullWidth>
+              Zapisz
+            </Button>
+          </Box>
+        </Box>
+        <Divider sx={{ my: 2 }} />
+      </Container>
+
+      <Container maxWidth="md">
         <Divider sx={{ my: 2 }} />
 
         {/* Accordion */}
@@ -336,12 +358,6 @@ const SurveysSettings = () => {
         {/* Divider */}
         <Divider sx={{ my: 2 }} />
       </Container>
-
-      <Box position="fixed" bottom={16} right={16}>
-        <Button variant="contained" color="primary" onClick={handleSave}>
-          Zapisz
-        </Button>
-      </Box>
     </>
   )
 }
