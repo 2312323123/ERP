78590aaaf97e9efd7eca2cfb46eaa5b85a185ebf
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-03 02:03:57 +0100

    [surveys] set required roles for endpoints

--- recruitment_survey_phase/src/app.controller.ts
+++ recruitment_survey_phase/src/app.controller.ts
@@ -22,21 +22,25 @@ export class AppController {
   //   return 'Hello World auth!';
   // }
 
+  @Roles('USER', 'SUPERADMIN')
   @Get('/api/surveys/active-recruitment-name-uuid')
   async getActiveRecruitmentNameUuid(): Promise<{ name: string; uuid: string } | undefined> {
     return this.appService.getActiveRecruitmentNameUuid();
   }
 
+  @Roles('RECRUITMENT_ADMIN')
   @Get('/api/surveys/all-recruitments-uuid-name-start-date')
   async getAllRecruitmentsUuidNameStartDate(): Promise<{ uuid: string; name: string; startDate: Date }[]> {
     return this.appService.getAllRecruitmentsUuidNameStartDate();
   }
 
   // one switch in survey settings
+  @Roles('USER', 'RECRUITMENT_ADMIN')
   @Get('/api/surveys/can-people-see-recruitment')
   async getCanPeopleSeeRecruitment(): Promise<{ can_people_see_recruitment: boolean }> {
     return this.appService.getCanPeopleSeeRecruitment();
   }
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/can-people-see-recruitment')
   async setCanPeopleSeeRecruitment(
     @Body('can_people_see_recruitment', UndefinedCheckPipe) can_people_see_recruitment: boolean,
@@ -45,25 +49,30 @@ export class AppController {
   }
 
   // one switch in survey settings
+  @Roles('USER', 'RECRUITMENT_ADMIN')
   @Get('/api/surveys/can-evaluate-surveys')
   async getCanEvaluateSurveys(): Promise<{ can_evaluate_surveys: boolean }> {
     return this.appService.getCanEvaluateSurveys();
   }
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/can-evaluate-surveys')
   async setCanEvaluateSurveys(@Body('can_evaluate_surveys', UndefinedCheckPipe) can_evaluate_surveys: boolean) {
     return this.appService.setCanEvaluateSurveys(new CreateCanEvaluateSurveyDto(can_evaluate_surveys));
   }
 
   // one switch in survey settings
+  @Roles('USER', 'RECRUITMENT_ADMIN')
   @Get('/api/surveys/accepts-surveys')
   async getAcceptsSurveys(): Promise<{ accepts_surveys: boolean }> {
     return this.appService.getAcceptsSurveys();
   }
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/accepts-surveys')
   async setAcceptsSurveys(@Body('accepts_surveys', UndefinedCheckPipe) accepts_surveys: boolean) {
     return this.appService.setAcceptsSurveys(new CreateAcceptsSurveyDto(accepts_surveys));
   }
 
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/create-recruitment')
   async createRecruitment(
     @Body('name', UndefinedCheckPipe) name: string,
@@ -79,11 +88,13 @@ export class AppController {
     );
   }
 
+  @Roles('USER', 'RECRUITMENT_ADMIN')
   @Get('/api/surveys/active-recruitment-settings')
   async getActiveRecruitmentSettings() {
     return this.appService.getActiveRecruitmentDataForFrontend();
   }
 
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/active-recruitment')
   async setActiveRecruitment(@Body('recruitment_uuid', UndefinedCheckPipe) recruitment_uuid: string) {
     if (!recruitment_uuid) {
@@ -92,12 +103,14 @@ export class AppController {
     return this.appService.setActiveRecruitment(recruitment_uuid);
   }
 
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/save-survey-settings')
   @HttpCode(201)
   async saveSurveySettings(@Body() surveySettings: Partial<RecruitmentRelatedData>) {
     await this.appService.saveSurveySettings(surveySettings);
   }
 
+  @Roles('RECRUITMENT_ADMIN')
   @Post('/api/surveys/delete-recruitment')
   async deleteRecruitment(@Body('uuid', UndefinedCheckPipe) uuid: string) {
     if (!uuid) {
