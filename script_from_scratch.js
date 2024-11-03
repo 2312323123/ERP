d8df6440b77c3e1ac6ab0bd76d549f4eaa133284
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-03 17:56:01 +0100

    [survey script] different logging

--- script_from_scratch.js
+++ script_from_scratch.js
@@ -1,18 +1,21 @@
+// [survey script] adsf
 function myFunc() {
   // Open a form by ID and log the responses to each question.
   var form = FormApp.openById("1hIh-1Lzyh8TBkRZcGseHEfib5LVdq10-0hcuiQclo9M");
   var formResponses = form.getResponses();
   for (var i = 0; i < formResponses.length; i++) {
+    // this I can work on
     var formResponse = formResponses[i];
     var itemResponses = formResponse.getItemResponses();
     for (var j = 0; j < itemResponses.length; j++) {
       var itemResponse = itemResponses[j];
       Logger.log(
-        'Response #%s to the question "%s" was "%s"',
-        (i + 1).toString(),
+        'question: "%s", response: "%s"',
         itemResponse.getItem().getTitle(),
         itemResponse.getResponse()
       );
     }
+
+    // end
   }
 }
