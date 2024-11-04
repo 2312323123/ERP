3199f36edc8d39e79c0088db5e9b6e54b1cc8944
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-04 09:24:44 +0100

    [frontend] alert when switching accepting surveys to 'off' that they should do the same in the form

--- frontend/src/hooks/surveys/useSetAcceptsSurveys.ts
+++ frontend/src/hooks/surveys/useSetAcceptsSurveys.ts
@@ -1,22 +1,29 @@
 import { useSetAcceptsSurveysMutation } from '../../services/erp'
 import { useSnackbar } from '../useSnackbar'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const useSetAcceptsSurveys = () => {
   const [setAcceptsSurveys, { isLoading, isSuccess, isError }] = useSetAcceptsSurveysMutation()
+  const [lastSwitchValue, setLastSwitchValue] = useState<boolean | null>(null)
   const showSnackbar = useSnackbar()
   useEffect(() => {
     if (isSuccess) {
       showSnackbar('Poprawnie zmieniono ustawienia!', 'success')
     }
+    if (isSuccess && !lastSwitchValue) {
+      setTimeout(() => alert('Teraz powinieneś/aś wyłączyć przyjmowanie ankiet również w formsie'), 200)
+    }
     if (isError) {
       showSnackbar('Wystąpił błąd przy zmianie ustawień!', 'error')
     }
-  }, [isSuccess, isError, showSnackbar])
+  }, [isSuccess, isError, showSnackbar, lastSwitchValue])
 
   return {
     isLoading,
-    callback: (value: boolean) => setAcceptsSurveys(value),
+    callback: (value: boolean) => {
+      setLastSwitchValue(value)
+      setAcceptsSurveys(value)
+    },
   }
 }
 
