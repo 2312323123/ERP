7238046b8619b712cfeff11f90681890a245c4ce
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-16 15:11:17 +0100

    [surveys] finally add getActiveRecruitmentUuid method

--- recruitment_survey_phase/src/active_recruitment/active_recruitment.service.ts
+++ recruitment_survey_phase/src/active_recruitment/active_recruitment.service.ts
@@ -1,6 +1,4 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { CreateActiveRecruitmentDto } from './dto/create-active_recruitment.dto';
-import { UpdateActiveRecruitmentDto } from './dto/update-active_recruitment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ActiveRecruitment } from './entities/active_recruitment.entity';
 import { Repository } from 'typeorm';
@@ -13,25 +11,9 @@ export class ActiveRecruitmentService {
     @InjectRepository(Recruitment) private recruitmentRepository: Repository<Recruitment>,
   ) {}
 
-  // create(createActiveRecruitmentDto: CreateActiveRecruitmentDto) {
-  //   return 'This action adds a new activeRecruitment';
-  // }
-
-  // findAll() {
-  //   return `This action returns all activeRecruitment`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} activeRecruitment`;
-  // }
-
-  // update(id: number, updateActiveRecruitmentDto: UpdateActiveRecruitmentDto) {
-  //   return `This action updates a #${id} activeRecruitment`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} activeRecruitment`;
-  // }
+  async getActiveRecruitmentUuid(): Promise<string | undefined> {
+    return (await this.activeRecruitmentRepository.find())?.[0]?.recruitment_uuid ?? undefined;
+  }
 
   async getActiveRecruitmentNameUuid(): Promise<{ name: string; uuid: string } | undefined> {
     const activeRecruitment = (
