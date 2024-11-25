5be6f5b1f5d0d2feaf957f47f4672504280a1949
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-25 01:17:53 +0100

    [frontend] avl - make resetting selection possible

--- frontend/src/components/AvlTest.tsx
+++ frontend/src/components/AvlTest.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@mui/material'
+import { set } from 'date-fns'
 import { useState } from 'react'
 import AvailableTimes from 'react-available-times'
 
@@ -25,52 +27,65 @@ const AvlTest = () => {
     { start: 200, end: 300 },
   ])
 
+  const [show, setShow] = useState(true)
+
+  const reset = () => {
+    setDupa([{ start: 0, end: 100 }])
+    setShow(false)
+    setTimeout(() => setShow(true), 0)
+  }
+
   return (
     <>
-      <AvailableTimes
-        weekStartsOn="monday"
-        calendars={[
-          {
-            id: 'work',
-            title: 'Work',
-            foregroundColor: '#ff00ff',
-            backgroundColor: '#f0f0f0',
-            selected: true,
-          },
-          {
-            id: 'private',
-            title: 'My private cal',
-            foregroundColor: '#666',
-            backgroundColor: '#f3f3f3',
-          },
-        ]}
-        onChange={(selections) => {
-          console.log('=======================')
-          // selections.forEach(({ start, end }) => {
-          //   console.log('Start:', start, 'End:', end)
-          // })
-          console.log(selections)
-          // setSelections(selections)
-          setDupa(selections)
-        }}
-        // initialSelections={selections}
-        // initialSelections={[{ start: 0, end: 100 }]}
-        initialSelections={dupa}
-        onEventsRequested={({ calendarId, start, end, callback }) => {
-          loadMoreEvents(calendarId, start, end) //.then(callback)
-        }}
-        // initialSelections={[{ start: aDateObject, end: anotherDateObject }]}
-        height={700}
-        recurring={true}
-        // availableDays={['monday', 'tuesday', 'wednesday', 'thursday', 'friday']}
-        // availableHourRange={{ start: 9, end: 19 }}
-      />
+      <div style={{ height: '700px' }}>
+        {show && (
+          <AvailableTimes
+            weekStartsOn="monday"
+            calendars={[
+              {
+                id: 'work',
+                title: 'Work',
+                foregroundColor: '#ff00ff',
+                backgroundColor: '#f0f0f0',
+                selected: true,
+              },
+              {
+                id: 'private',
+                title: 'My private cal',
+                foregroundColor: '#666',
+                backgroundColor: '#f3f3f3',
+              },
+            ]}
+            onChange={(selections) => {
+              console.log('=======================')
+              // selections.forEach(({ start, end }) => {
+              //   console.log('Start:', start, 'End:', end)
+              // })
+              console.log(selections)
+              // setSelections(selections)
+              setDupa(selections)
+            }}
+            // initialSelections={selections}
+            // initialSelections={[{ start: 0, end: 100 }]}
+            initialSelections={dupa}
+            onEventsRequested={({ calendarId, start, end, callback }) => {
+              loadMoreEvents(calendarId, start, end) //.then(callback)
+            }}
+            // initialSelections={[{ start: aDateObject, end: anotherDateObject }]}
+            height={700}
+            recurring={true}
+            // availableDays={['monday', 'tuesday', 'wednesday', 'thursday', 'friday']}
+            // availableHourRange={{ start: 9, end: 19 }}
+          />
+        )}
+      </div>
       <div>
         <h4>dupa</h4>
         {dupa.map((el) => (
           <div>{JSON.stringify(el)}</div>
         ))}
       </div>
+      <Button onClick={reset}>Reset</Button>
     </>
   )
 }
