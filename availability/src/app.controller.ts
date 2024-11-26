58e5de5d9846123c1e917929994ffaf690ef68d2
Maciej Piotrowski <maciej.piotrowski@best-eu.org>
2024-11-26 01:55:03 +0100

    [availability] prepare endpoints

--- availability/src/app.controller.ts
+++ availability/src/app.controller.ts
@@ -1,36 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Roles } from './auth/roles.decorator';
+import {
+  _Availability,
+  CreateAvailabilityDto,
+  UserAvailabilityInfo,
+} from './availabilities/dto/create-availability.dto';
+import { UserId } from './auth/user-id.decorator';
+import { UndefinedCheckPipe } from './pipes/undefined-check.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Roles('USER, SUPERADMIN')
+  @Roles('USER')
+  @Post('/api/availability')
+  async updateAvailability(
+    @UserId() userId: string,
+    @Body('availability', UndefinedCheckPipe) availability: _Availability,
+  ): Promise<void> {
+    const createAvailabilityDto = new CreateAvailabilityDto();
+    createAvailabilityDto.id = userId;
+    createAvailabilityDto.availability = availability;
+    return this.appService.updateAvailability(createAvailabilityDto);
+  }
+
+  @Roles('USER')
   @Get('/api/availability')
-  getHello(): string {
-    return this.appService.getHello();
+  async getAvailability(@Query('ids', UndefinedCheckPipe) ids: string | string[]): Promise<UserAvailabilityInfo[]> {
+    if (typeof ids === 'string') {
+      ids = [ids];
+    }
+    if (!Array.isArray(ids) || ids.length === 0) {
+      throw new BadRequestException("Invalid input: 'ids' must be a non-empty array");
+    }
+    return this.appService.getAvailability(ids);
   }
-
-  /*
-  @Post('/api/auth/setup-roles')
-  async setupRoles(@Body() { role, description = '' }: { role: string; description: string }) {
-    const createRoleDto = new CreateRoleDto(role, description);
-
-    return await this.rolesService.create(createRoleDto);
-  }
-got replaced by this:
-
-  @Post('/api/auth/setup-roles')
-  async setupRoles(
-    @Body('role', UndefinedCheckPipe) role: string,
-    @Body() { description = '' }: { description: string },
-  ) {
-    const createRoleDto = new CreateRoleDto(role, description);
-
-    return await this.rolesService.create(createRoleDto);
-  }
-*/
-
-  // async getSmth(@UserId() userId: string) {}
 }
--- availability/src/availabilities/dto/create-availability.dto.ts
+++ availability/src/availabilities/dto/create-availability.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAvailabilityDto {}
+export type _Availability = {
+  start: number;
+  end: number;
+}[];
+
+export class CreateAvailabilityDto {
+  id: string;
+  availability: _Availability;
+}
+
+export class UserAvailabilityInfo {
+  id: string;
+  availability: _Availability;
+}
