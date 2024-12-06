551444b5f30a22a0e149219c45d8ca7194453105
Maciej Piotrowski <2312323@protonmail.com>
2024-12-06 23:30:57 +0100

    [frontend] undo the damage done by PWA attempt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
-import { manifestForPlugIn } from './src/config/PWAmanifest'
 import { defineConfig } from 'vite'
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn as Partial<VitePWAOptions>)],
+  plugins: [react()],
   preview: {
     port: 8080,
     strictPort: true,
